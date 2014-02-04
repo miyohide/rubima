60845a97cf240f6866e8002e2a8a4c5adaec5887
Hidenori Miyoshi <miyohide@gmail.com>
2014-02-04 20:04:05 +0900

    ascii文字の前後にスペースが有ることを確認

---
+++ tools/test/rubima-lint_test.rb
@@ -0,0 +1,38 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'open3'
+
+describe "Rubima-Lint" do
+
+   describe "space of ascii characters before and after" do
+      describe "space of ascii characters before" do
+         it do
+            Open3.popen3("ruby rubima-lint.rb") { |stdin, stdout, stderr|
+               stdin.puts "アスキー文字の前にスペースabc が必要です"
+               stdin.close
+               stdout.read.must_match /1 warning/m
+            }
+         end
+      end
+
+      describe "space of ascii characters after" do
+         it do
+            Open3.popen3("ruby rubima-lint.rb") { |stdin, stdout, stderr|
+               stdin.puts "アスキー文字の前にスペース abcが必要です"
+               stdin.close
+               stdout.read.must_match /1 warning/m
+            }
+         end
+      end
+
+      describe "space of ascii characters before and after" do
+         it do
+            Open3.popen3("ruby rubima-lint.rb") { |stdin, stdout, stderr|
+               stdin.puts "アスキー文字の前にスペースabcが必要です"
+               stdin.close
+               stdout.read.must_match /2 warning/m
+            }
+         end
+      end
+   end
+end
